18f8ad3b4dc61efaec5ce3aac460472fb6d1c23f
KostiaChardonnet <kostia.chardonnet@yahoo.fr>
2016-12-19 13:35:24 +0100

    fix

--- client/client.c
+++ client/client.c
@@ -9,14 +9,20 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
-char* network_path "../network/Serveur.pipe";
+char* network_path = "../network/Serveur.pipe";
 char* my_pipe = "";
 int id;
-int* pipes = (int*) malloc (sizeof(int)*2);
+int *pipes;
 
 void run()
 {
-  connexion();
+	pipes = (int*) malloc (sizeof(int) * 2);
+	connexion();
+}
+
+char* get_message(size_t t)
+{
+	return NULL;
 }
 
 void connexion()
@@ -24,41 +30,41 @@ void connexion()
 
 	mode_t t = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH | S_IWOTH;
 
-	int mkfifo1 = 0;
 	if(access (network_path, F_OK) == -1){
-	  printf("le serveur n'existe pas");
-	  exit(-1);
+		printf("le serveur n'existe pas");
+		exit(-1);
 	}
 
 	pipes[0] = open(network_path, O_WRONLY);
 	pipes[1] = open(my_pipe, O_RDONLY);
 
-
-	pipe(pipes[1]);
 	size_t size = 16;
-	char* buff
+	char* buff;
 	do
 	{
 		printf("Veuillez choisir un pseudo : ");
 		if((buff = get_message(size)) == NULL) // pas d'input de l'utilisateur
 			continue;
-    
-	}while(connexion_approval(buff) == -1)
+
+	}while(connexion_approval(buff) == -1);
 
 }
 
 //FIXME : envoyer le msg encodé + récup id + chemin de tube perso et l'ouvrir en lecture.
 int connexion_approval(char* pseudo)
 {
-  write(pipes[0], pseudo, strlen(pseudo) * sizeof(char));
+
+	protocol_message connexion = encodeConnexion(pseudo, pseudo);
+
+	write(pipes[0], pseudo, strlen(pseudo) * sizeof(char));
 	char buff[4];
 
-	read(pipes[0], ?, ?);
+	//read(pipes[0], ?, ?);
 
 	if(strcmp(buff, "OKOK") == 0)
 	{
-	  id = ?
-	  return 1
+		id = 0;
+		return 1;
 	}
 
 	return -1;
@@ -76,41 +82,49 @@ void deconnexion()
 
 void read_message()
 {
-  int msg_length;
-  char* msg;
+	int msg_length;
+	char* msg;
 
-  char c;
-  int tmp = 1
-  char* tmp_size = (char*) calloc (1, 1);
-  while(1) //number for ascii
-  {
-    read(pipes[1], &c, strlen(char));
-    if(c < 48 || c > 57) // c is not a number, we got the full length
-      break;
-    tmp++;
-    tmp_size = (char*) realloc (tmp_size, tmp);
-    tmp_size[tmp-1] = c;
-  }
+	char c;
+	int tmp = 1;	
+	char* tmp_size = (char*) calloc (1, 1);
+	while(1) //number for ascii
+	{
 
-  msg_length = atoi(size);
-  msg = (char*) calloc (msg_length, sizeof(char));
+	  	read(pipes[1], &c, sizeof(char));
+	    if(c < 48 || c > 57) // c is not a number, we got the full length
+	    	break;
+	    tmp++;
+	    tmp_size = (char*) realloc (tmp_size, tmp);
+	    tmp_size[tmp-1] = c;
 
-  read(pipes[1], msg, msg_length);
+	}
+
+	msg_length = atoi(tmp_size);
+	msg = (char*) calloc (msg_length, sizeof(char));
+
+	read(pipes[1], msg, msg_length);
 }
 
-void send_message(const char* msg)
+void send_message(char* msg)
 {
-  char* code = encode(msg);
-  write(pipes[0], code, strlen(code));
+	char* code = encode(msg);
+	write(pipes[0], code, strlen(code));
 }
 
 void print_message(const char* msg)
 {
-  printf("%s", parsing(msg));
+	printf("%s", parsing(msg));
 }
 
 // Coloriage du texte, gestion des pseudo pour les messages privé, etc.
-char* parsing(char* msg)
+char* parsing(const char* msg)
 {
-  return NULL;
+	return NULL;
+}
+
+int main()
+{
+	run();
+	return 0;
 }
--- client/client.h
+++ client/client.h
@@ -1,6 +1,14 @@
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <time.h>
+#include <sys/file.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "../protocol/protocol.h"
+#include "protocol_primitives.h"
+#include "protocol_instance.h"
 
 void run();
 void connexion();
@@ -8,5 +16,5 @@ int connexion_approval(char* pseudo);
 void deconnexion();
 void read_message();
 void send_message();
-void print_message(char* msg);
-char* parsing(char* msg);
+void print_message(const char* msg);
+char* parsing(const char* msg);
